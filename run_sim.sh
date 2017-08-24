72311513ddf5a1da569089cf7e2e6df141700b41
Martin Robinson <martinjrobins@gmail.com>
2017-08-24 11:35:08 +0100

    update run script

--- run_sim.sh
+++ run_sim.sh
@@ -10,9 +10,31 @@ fibre_number=10
 fibre_number_random=100
 electrostatics_fibre=1
 fibres_charge=0.1
+fibres_charges=(0.0001 0.001 0.01 0.1)
 
 echo "blah"
-nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --domain_size ${fibre_number} --base_dir "regular_" &> run_sim_regular.out &
-nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "hexagon_" &> run_sim_hexagon.out &
-nohup nice ./simulation --fibre_number ${fibre_number_random} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 2 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "random_" &> run_sim_random.out &
+for electrostatics_fibre in 1 0
+do
+    if [ $electrostatics_fibre -eq 1 ] 
+    then
+        fibres_charges=(0.0001 0.001 0.01 0.1)
+    else
+        fibres_charges=(0.0)
+    fi 
+    for fibres_charge in ${fibres_charges[@]}   
+    do
+        dirname="electro_${electrostatics_fibre}_${fibres_charge}"
+        mkdir -p $dirname
+        echo "electro = $electrostatics_fibre charge = $fibres_charge"
+        echo "doing regular..."
+        #nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --domain_size ${fibre_number} --base_dir "${dirname}/regular_" &> ${dirname}/run_sim_regular.out &
+        echo "doing hexagon..."
+        #nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out &
+        for i in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+        do
+            echo "doing ${i}random..."
+            #nohup nice ./simulation --seed $i --fibre_number ${fibre_number_random} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 2 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/${i}random_" &> ${dirname}/run_sim_${i}rand.out &
+        done
+    done
+done
 
