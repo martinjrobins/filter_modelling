5f47fec8320bf139c26a85f29b9b8748e9a9eff3
Martin Robinson <martinjrobins@gmail.com>
2017-08-24 12:16:05 +0100

    setup for arcus

--- run_sim.sh
+++ run_sim.sh
@@ -13,6 +13,7 @@ fibres_charge=0.1
 fibres_charges=(0.0001 0.001 0.01 0.1)
 
 echo "blah"
+COUNTER=0
 for electrostatics_fibre in 1 0
 do
     if [ $electrostatics_fibre -eq 1 ] 
@@ -27,13 +28,16 @@ do
         mkdir -p $dirname
         echo "electro = $electrostatics_fibre charge = $fibres_charge"
         echo "doing regular..."
-        #nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --domain_size ${fibre_number} --base_dir "${dirname}/regular_" &> ${dirname}/run_sim_regular.out &
+        echo -e "#!/bin/bash'\n'./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --domain_size ${fibre_number} --base_dir "${dirname}/regular_" &> ${dirname}/run_sim_regular.out" > job_$COUNTER.sh
+        COUNTER=$((COUNTER + 1))
         echo "doing hexagon..."
-        #nohup nice ./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out &
+        echo -e "#!/bin/bash'\n'./simulation --fibre_number ${fibre_number} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out" > job_$COUNTER.sh 
+        COUNTER=$((COUNTER + 1))
         for i in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
         do
             echo "doing ${i}random..."
-            #nohup nice ./simulation --seed $i --fibre_number ${fibre_number_random} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 2 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/${i}random_" &> ${dirname}/run_sim_${i}rand.out &
+            echo -e "#!/bin/bash\n./simulation --seed $i --fibre_number ${fibre_number_random} --nx ${nx} --fibre_resolution ${fr} --c0 ${c0} --fibre_arrangement 2 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --base_dir "${dirname}/${i}random_" &> ${dirname}/run_sim_${i}rand.out" > job_$COUNTER.sh
+            COUNTER=$((COUNTER + 1))
         done
     done
 done
