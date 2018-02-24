680bb42adc0b2add0a3f9bee9d47d85435ac8406
Martin Robinson <martinjrobins@gmail.com>
2018-02-24 07:22:57 +0000

    print seconds

--- run_sim.sh
+++ run_sim.sh
@@ -35,19 +35,19 @@ do
             if [ $electrostatics_fibre -eq 0 ] 
             then
                 echo "doing regular..."
-                echo -e "#!/bin/bash\n./simulation --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --domain_size ${fibre_number} --base_dir "${dirname}/regular_" &> ${dirname}/run_sim_regular.out" > job_$COUNTER.sh
+                echo -e "#!/bin/bash\n./simulation --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 0 --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --domain_size ${fibre_number} --base_dir "${dirname}/regular_" &> ${dirname}/run_sim_regular.out\necho \$SECONDS" > job_$COUNTER.sh
                 echo -e "#!/bin/bash\ncd ${dirname}\npython ../../vis.py" > job_vis_$VIS_COUNTER.sh
                 COUNTER=$((COUNTER + 1))
                 VIS_COUNTER=$((VIS_COUNTER + 1))
 
                 echo "doing hexagon..."
-                echo -e "#!/bin/bash\n./simulation --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out" > job_$COUNTER.sh 
+                echo -e "#!/bin/bash\n./simulation --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out\necho \$SECONDS" > job_$COUNTER.sh 
                 COUNTER=$((COUNTER + 1))
             else
                 for i in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
                 do
                     echo "doing ${i}hexagon..."
-                    echo -e "#!/bin/bash\n./simulation --seed $i --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out" > job_$COUNTER.sh 
+                    echo -e "#!/bin/bash\n./simulation --seed $i --fibre_number ${fibre_number} --nx ${nx}  --fibre_arrangement 1 --domain_size ${fibre_number} --electrostatics_fibre ${electrostatics_fibre} --fibres_charge ${fibres_charge} --fibres_charge_stddev ${fibres_charge_stddev} --base_dir "${dirname}/hexagon_" &> ${dirname}/run_sim_hexagon.out\necho \$SECONDS" > job_$COUNTER.sh 
                     COUNTER=$((COUNTER + 1))
                 done
             fi
